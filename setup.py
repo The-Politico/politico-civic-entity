318cd14c407e8e650374f784e692923798eacd81
Tyler Fisher <tfisher@politico.com>
2019-01-04 11:17:06 -0500

    0.3.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="politico-civic-entity",
-    version="0.3.0",
+    version="0.3.1",
     description="",
     url="https://github.com/The-Politico/politico-civic-entity",
     author="POLITICO interactive news",
