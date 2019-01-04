b21ec328db2f83e9e40dc42a919c561fa32e25dd
Tyler Fisher <tfisher@politico.com>
2019-01-04 11:10:03 -0500

    make person image a url field

--- entity/models/person_image.py
+++ entity/models/person_image.py
@@ -3,22 +3,9 @@ import uuid
 
 from django.db import models
 from django.utils.html import format_html
-from entity.utils.aws import StorageService
 
 from .image_tag import ImageTag
 from .person import Person
-
-
-def person_image_path(instance, filename):
-    return os.path.join(
-        'cdn/images/people',
-        instance.person.slug,
-        '{}-{}{}'.format(
-            instance.tag,
-            uuid.uuid4().hex[:6],
-            os.path.splitext(filename)[1]
-        )
-    )
 
 
 class PersonImage(models.Model):
@@ -26,16 +13,17 @@ class PersonImage(models.Model):
     Image attached to a person, which can be serialized
     by a tag.
     """
+
     person = models.ForeignKey(
-        Person, related_name='images', on_delete=models.PROTECT)
-    tag = models.ForeignKey(
-        ImageTag, related_name="+",
-        on_delete=models.PROTECT,
-        help_text="Used to serialize images.")
-    image = models.ImageField(
-        upload_to=person_image_path,
-        storage=StorageService()
+        Person, related_name="images", on_delete=models.PROTECT
     )
+    tag = models.ForeignKey(
+        ImageTag,
+        related_name="+",
+        on_delete=models.PROTECT,
+        help_text="Used to serialize images.",
+    )
+    image = models.URLField()
 
     created = models.DateTimeField(auto_now_add=True, editable=False)
     updated = models.DateTimeField(auto_now=True, editable=False)
@@ -44,11 +32,11 @@ class PersonImage(models.Model):
         return format_html(
             '<a href="{0}" target="_blank">'
             '<img src="{0}" style="max-height:100px; max-width: 300px;">'
-            '</a>'.format(self.image.url)
+            "</a>".format(self.image.url)
         )
 
     class Meta:
-        unique_together = ('person', 'tag')
+        unique_together = ("person", "tag")
 
     def __str__(self):
-        return '{} {}'.format(self.person.slug, self.tag.name)
+        return "{} {}".format(self.person.slug, self.tag.name)
