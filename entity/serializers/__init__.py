ac5e4d106a9586f515ee46f1fe4b4d42d41af3ea
Allan James Vestal <allanjamesvestal@gmail.com>
2019-07-10 16:50:38 -0500

    Scope all allowable imports from 'entity.serializers' module.

--- entity/serializers/__init__.py
+++ entity/serializers/__init__.py
@@ -1,5 +1,20 @@
 # flake8: noqa
-from .image_tag import ImageTagSerializer
-from .organization_classification import OrganizationClassificationSerializer
-from .organization import OrganizationSerializer
-from .person import PersonSerializer
+# Imports from entity.
+from entity.serializers.image_tag import ImageTagSerializer
+from entity.serializers.organization_classification import (
+    OrganizationClassificationSerializer
+)
+from entity.serializers.organization_image import OrganizationImageSerializer
+from entity.serializers.organization import OrganizationSerializer
+from entity.serializers.person_image import PersonImageSerializer
+from entity.serializers.person import PersonSerializer
+
+
+__all__ = [
+    'ImageTagSerializer',
+    'OrganizationClassificationSerializer',
+    'OrganizationImageSerializer',
+    'OrganizationSerializer',
+    'PersonImageSerializer',
+    'PersonSerializer',
+]
