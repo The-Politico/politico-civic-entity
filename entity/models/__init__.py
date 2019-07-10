937c939eaa120fa3a41af42d04a3f913f62e89c1
Allan James Vestal <allanjamesvestal@gmail.com>
2019-07-10 16:37:16 -0500

    Scope all allowable imports from 'entity.models' module.

--- entity/models/__init__.py
+++ entity/models/__init__.py
@@ -1,7 +1,19 @@
 # flake8: noqa
 from entity.models.image_tag import ImageTag
-from entity.models.organization_classification import OrganizationClassification
+from entity.models.organization_classification import (
+    OrganizationClassification
+)
 from entity.models.organization_image import OrganizationImage
 from entity.models.organization import Organization
 from entity.models.person_image import PersonImage
 from entity.models.person import Person
+
+
+__all__ = [
+    'ImageTag',
+    'Organization',
+    'OrganizationClassification',
+    'OrganizationImage',
+    'Person',
+    'PersonImage',
+]
